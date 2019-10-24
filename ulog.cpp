32135a45f1ca5c78af64c557395dafa14c20342a
fengshun <fengshun95@gmail.com>
2019-10-24 19:25:14 +0800

    Format code style to google C++ style

--- ulog.cpp
+++ ulog.cpp
@@ -37,10 +37,10 @@ static LoggerLevel log_output_level_ = ULOG_DEFAULT_LEVEL;
 #endif
 
 enum {
-    INDEX_PRIMARY_COLOR = 0,
-    INDEX_SECONDARY_COLOR,
-    INDEX_LEVEL_MARK,
-    INDEX_MAX,
+  INDEX_PRIMARY_COLOR = 0,
+  INDEX_SECONDARY_COLOR,
+  INDEX_LEVEL_MARK,
+  INDEX_MAX,
 };
 
 static char *level_infos[ULOG_LEVEL_NUMBER][INDEX_MAX]{
@@ -53,46 +53,46 @@ static char *level_infos[ULOG_LEVEL_NUMBER][INDEX_MAX]{
 };
 
 class LockGuard {
-   public:
-    explicit LockGuard(pthread_mutex_t &_m) : mMutex(_m) {
-        pthread_mutex_lock(&mMutex);
-    }
+ public:
+  explicit LockGuard(pthread_mutex_t &_m) : mMutex(_m) {
+    pthread_mutex_lock(&mMutex);
+  }
 
-    ~LockGuard() { pthread_mutex_unlock(&mMutex); }
+  ~LockGuard() { pthread_mutex_unlock(&mMutex); }
 
-   private:
-    pthread_mutex_t &mMutex;
+ private:
+  pthread_mutex_t &mMutex;
 };
 
 #endif  // !ULOG_DISABLE
 
 void logger_enable_output(uint8_t enable) {
 #if !defined(ULOG_DISABLE)
-    log_output_enable_ = (enable != 0);
+  log_output_enable_ = (enable != 0);
 #endif
 }
 
 void logger_enable_color(uint8_t enable) {
 #if !defined(ULOG_DISABLE)
-    log_color_enable_ = (enable != 0);
+  log_color_enable_ = (enable != 0);
 #endif
 }
 
 void logger_set_output_level(LoggerLevel level) {
 #if !defined(ULOG_DISABLE)
-    log_output_level_ = level;
+  log_output_level_ = level;
 #endif
 }
 
 void logger_init(LogOutputCb output_cb) {
 #if !defined(ULOG_DISABLE)
 
-    pthread_mutex_init(&log_lock_, nullptr);
-    output_cb_ = output_cb;
+  pthread_mutex_init(&log_lock_, nullptr);
+  output_cb_ = output_cb;
 
 #if defined(ULOG_CLS)
-    char clear_str[3] = {'\033', 'c', '\0'};  // clean screen
-    if (output_cb) output_cb(clear_str);
+  char clear_str[3] = {'\033', 'c', '\0'};  // clean screen
+  if (output_cb) output_cb(clear_str);
 #endif
 
 #endif
@@ -102,57 +102,57 @@ void logger_log(LoggerLevel level, const char *file, const char *func, int line,
                 const char *fmt, ...) {
 #if !defined(ULOG_DISABLE)
 
-    if (!output_cb_ || !fmt || level < log_output_level_ || !log_output_enable_)
-        return;
+  if (!output_cb_ || !fmt || level < log_output_level_ || !log_output_enable_)
+    return;
 
-    LockGuard lock = LockGuard(log_lock_);
+  LockGuard lock = LockGuard(log_lock_);
 
-    char *buf_ptr = log_out_buf_;
-    // The last three characters are '\r', '\n', '\0'
-    char *buf_end_ptr = log_out_buf_ + LOG_OUTBUF_LEN - 3;
+  char *buf_ptr = log_out_buf_;
+  // The last three characters are '\r', '\n', '\0'
+  char *buf_end_ptr = log_out_buf_ + LOG_OUTBUF_LEN - 3;
 
-    char *log_info_color = log_color_enable_
-                               ? level_infos[level][INDEX_SECONDARY_COLOR]
-                               : (char *)"";
+  char *log_info_color = log_color_enable_
+                             ? level_infos[level][INDEX_SECONDARY_COLOR]
+                             : (char *)"";
 
-    /* Print serial number */
-    snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "%s#%06u ", log_info_color,
-             log_evt_num_++);
-    buf_ptr = log_out_buf_ + strlen(log_out_buf_);
+  /* Print serial number */
+  snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "%s#%06u ", log_info_color,
+           log_evt_num_++);
+  buf_ptr = log_out_buf_ + strlen(log_out_buf_);
 
-    // Print time
-    struct timespec tsp {};
-    clock_gettime(CLOCK_MONOTONIC, &tsp);
-    snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "[%lu.%03lu] ", tsp.tv_sec,
-             tsp.tv_nsec / (1000 * 1000));
-    buf_ptr = log_out_buf_ + strlen(log_out_buf_);
+  // Print time
+  struct timespec tsp {};
+  clock_gettime(CLOCK_MONOTONIC, &tsp);
+  snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "[%lu.%03lu] ", tsp.tv_sec,
+           tsp.tv_nsec / (1000 * 1000));
+  buf_ptr = log_out_buf_ + strlen(log_out_buf_);
 
-    // Print level, file, function and line
-    char *level_mark = level_infos[level][INDEX_LEVEL_MARK];
-    char *info_str_color = (char *)(log_color_enable_ ? STR_GRAY : "");
-    snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "%s%s/(%s:%d %s) ", level_mark,
-             info_str_color, file, line, func);
-    output_cb_(log_out_buf_);
-    buf_ptr = log_out_buf_;
+  // Print level, file, function and line
+  char *level_mark = level_infos[level][INDEX_LEVEL_MARK];
+  char *info_str_color = (char *)(log_color_enable_ ? STR_GRAY : "");
+  snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "%s%s/(%s:%d %s) ", level_mark,
+           info_str_color, file, line, func);
+  output_cb_(log_out_buf_);
+  buf_ptr = log_out_buf_;
 
-    // Print log info
-    snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "%s", log_info_color);
-    buf_ptr = log_out_buf_ + strlen(log_out_buf_);
+  // Print log info
+  snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "%s", log_info_color);
+  buf_ptr = log_out_buf_ + strlen(log_out_buf_);
 
-    va_list ap;
-    va_start(ap, fmt);
-    vsnprintf(buf_ptr, (buf_end_ptr - buf_ptr), fmt, ap);
-    buf_ptr = log_out_buf_ + strlen(log_out_buf_);
-    va_end(ap);
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(buf_ptr, (buf_end_ptr - buf_ptr), fmt, ap);
+  buf_ptr = log_out_buf_ + strlen(log_out_buf_);
+  va_end(ap);
 
-    char *str_reset_color = (char *)(log_color_enable_ ? STR_RESET : "");
-    snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "%s", str_reset_color);
-    buf_ptr = log_out_buf_ + strlen(log_out_buf_);
+  char *str_reset_color = (char *)(log_color_enable_ ? STR_RESET : "");
+  snprintf(buf_ptr, (buf_end_ptr - buf_ptr), "%s", str_reset_color);
+  buf_ptr = log_out_buf_ + strlen(log_out_buf_);
 
-    *buf_ptr++ = '\r';
-    *buf_ptr++ = '\n';
-    *buf_ptr = '\0';
+  *buf_ptr++ = '\r';
+  *buf_ptr++ = '\n';
+  *buf_ptr = '\0';
 
-    output_cb_(log_out_buf_);
+  output_cb_(log_out_buf_);
 #endif
 }
--- ulog.h
+++ ulog.h
@@ -33,12 +33,12 @@
 // Precompiler define to get only filename;
 #if !defined(__FILENAME__)
 #define __FILENAME__ \
-    (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+  (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #endif
 
 #if !defined(ULOG_DISABLE)
 #define _logger_log(level, ...) \
-    logger_log(level, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+  logger_log(level, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
 #define _logger_log(level, ...)
 #endif
@@ -58,61 +58,56 @@
 #endif
 
 #define LOG_TOKEN(token)                                                      \
-    do {                                                                      \
-        if (_TYPE_CMP(token, float) || _TYPE_CMP(token, double)) {            \
-            LOG_DEBUG("(float) %s => %f", #token, token);                     \
-            /* Types with symbols below 64 bits */                            \
-        } else if (_TYPE_CMP(token, char) ||                                  \
-                   _TYPE_CMP(token, unsigned char) ||                         \
-                   _TYPE_CMP(token, short) ||                                 \
-                   _TYPE_CMP(token, unsigned short) ||                        \
-                   _TYPE_CMP(token, int) || _TYPE_CMP(token, unsigned int) || \
-                   _TYPE_CMP(token, long) || _TYPE_CMP(token, long long)) {   \
-            LOG_DEBUG("(int) %s => %" PRId64, #token, (int64_t)(token));      \
-            /* May be a 64-bit type */                                        \
-        } else if (_TYPE_CMP(token, unsigned long) ||                         \
-                   _TYPE_CMP(token, unsigned long long)) {                    \
-            LOG_DEBUG("(int) %s => %" PRIu64, #token, (uint64_t)(token));     \
-        } else if (_TYPE_CMP(token, char *) ||                                \
-                   _TYPE_CMP(token, unsigned char *) ||                       \
-                   _TYPE_CMP(token, char[]) ||                                \
-                   _TYPE_CMP(token, unsigned char[])) {                       \
-            LOG_DEBUG("(char*) %s => %s", #token, token);                     \
-        } else if (_TYPE_CMP(token, void *) || _TYPE_CMP(token, short *) ||   \
-                   _TYPE_CMP(token, unsigned short *) ||                      \
-                   _TYPE_CMP(token, int *) ||                                 \
-                   _TYPE_CMP(token, unsigned int *) ||                        \
-                   _TYPE_CMP(token, long *) ||                                \
-                   _TYPE_CMP(token, unsigned long *) ||                       \
-                   _TYPE_CMP(token, long long *) ||                           \
-                   _TYPE_CMP(token, unsigned long long *) ||                  \
-                   _TYPE_CMP(token, float *) || _TYPE_CMP(token, double *)) { \
-            LOG_DEBUG("(void *) %s => %x", #token, token);                    \
-        } else {                                                              \
-            LOG_DEBUG("(none) %s = (unknown)", #token);                       \
-        }                                                                     \
-    } while (0)
+  do {                                                                        \
+    if (_TYPE_CMP(token, float) || _TYPE_CMP(token, double)) {                \
+      LOG_DEBUG("(float) %s => %f", #token, token);                           \
+      /* Types with symbols below 64 bits */                                  \
+    } else if (_TYPE_CMP(token, char) || _TYPE_CMP(token, unsigned char) ||   \
+               _TYPE_CMP(token, short) || _TYPE_CMP(token, unsigned short) || \
+               _TYPE_CMP(token, int) || _TYPE_CMP(token, unsigned int) ||     \
+               _TYPE_CMP(token, long) || _TYPE_CMP(token, long long)) {       \
+      LOG_DEBUG("(int) %s => %" PRId64, #token, (int64_t)(token));            \
+      /* May be a 64-bit type */                                              \
+    } else if (_TYPE_CMP(token, unsigned long) ||                             \
+               _TYPE_CMP(token, unsigned long long)) {                        \
+      LOG_DEBUG("(int) %s => %" PRIu64, #token, (uint64_t)(token));           \
+    } else if (_TYPE_CMP(token, char *) ||                                    \
+               _TYPE_CMP(token, unsigned char *) ||                           \
+               _TYPE_CMP(token, char[]) ||                                    \
+               _TYPE_CMP(token, unsigned char[])) {                           \
+      LOG_DEBUG("(char*) %s => %s", #token, token);                           \
+    } else if (_TYPE_CMP(token, void *) || _TYPE_CMP(token, short *) ||       \
+               _TYPE_CMP(token, unsigned short *) ||                          \
+               _TYPE_CMP(token, int *) || _TYPE_CMP(token, unsigned int *) || \
+               _TYPE_CMP(token, long *) ||                                    \
+               _TYPE_CMP(token, unsigned long *) ||                           \
+               _TYPE_CMP(token, long long *) ||                               \
+               _TYPE_CMP(token, unsigned long long *) ||                      \
+               _TYPE_CMP(token, float *) || _TYPE_CMP(token, double *)) {     \
+      LOG_DEBUG("(void *) %s => %x", #token, token);                          \
+    } else {                                                                  \
+      LOG_DEBUG("(none) %s = (unknown)", #token);                             \
+    }                                                                         \
+  } while (0)
 
 #define _LOG_TIME_FUNCTION_LENGTH 50
 
-#define LOG_TIME_CODE(function)                                        \
-    do {                                                               \
-        struct timespec ulog_time_tsp1 = {};                           \
-        struct timespec ulog_time_tsp2 = {};                           \
-        clock_gettime(CLOCK_MONOTONIC, &ulog_time_tsp1);               \
-        function;                                                      \
-        clock_gettime(CLOCK_MONOTONIC, &ulog_time_tsp2);               \
-        float ulog_time_timediff =                                     \
-            (ulog_time_tsp2.tv_sec - ulog_time_tsp1.tv_sec) +          \
-            (float)(ulog_time_tsp2.tv_nsec - ulog_time_tsp1.tv_nsec) / \
-                (1000 * 1000 * 1000);                                  \
-        char ulog_time_function_str[_LOG_TIME_FUNCTION_LENGTH];        \
-        memset(ulog_time_function_str, 0, _LOG_TIME_FUNCTION_LENGTH);  \
-        strncpy(ulog_time_function_str, #function,                     \
-                _LOG_TIME_FUNCTION_LENGTH - 1);                        \
-        LOG_DEBUG("time { %s }: %fs", ulog_time_function_str,          \
-                  ulog_time_timediff);                                 \
-    } while (0)
+#define LOG_TIME_CODE(function)                                                \
+  do {                                                                         \
+    struct timespec ulog_time_tsp1 = {};                                       \
+    struct timespec ulog_time_tsp2 = {};                                       \
+    clock_gettime(CLOCK_MONOTONIC, &ulog_time_tsp1);                           \
+    function;                                                                  \
+    clock_gettime(CLOCK_MONOTONIC, &ulog_time_tsp2);                           \
+    float ulog_time_timediff =                                                 \
+        (ulog_time_tsp2.tv_sec - ulog_time_tsp1.tv_sec) +                      \
+        (float)(ulog_time_tsp2.tv_nsec - ulog_time_tsp1.tv_nsec) /             \
+            (1000 * 1000 * 1000);                                              \
+    char ulog_time_function_str[_LOG_TIME_FUNCTION_LENGTH];                    \
+    memset(ulog_time_function_str, 0, _LOG_TIME_FUNCTION_LENGTH);              \
+    strncpy(ulog_time_function_str, #function, _LOG_TIME_FUNCTION_LENGTH - 1); \
+    LOG_DEBUG("time { %s }: %fs", ulog_time_function_str, ulog_time_timediff); \
+  } while (0)
 
 #ifdef __cplusplus
 extern "C" {
@@ -121,13 +116,13 @@ extern "C" {
 typedef int (*LogOutputCb)(const char *ptr);
 
 typedef enum {
-    ULOG_VERBOSE = 0,
-    ULOG_DEBUG,
-    ULOG_INFO,
-    ULOG_WARN,
-    ULOG_ERROR,
-    ULOG_ASSERT,
-    ULOG_LEVEL_NUMBER
+  ULOG_VERBOSE = 0,
+  ULOG_DEBUG,
+  ULOG_INFO,
+  ULOG_WARN,
+  ULOG_ERROR,
+  ULOG_ASSERT,
+  ULOG_LEVEL_NUMBER
 } LoggerLevel;
 
 void logger_enable_output(uint8_t enable);
