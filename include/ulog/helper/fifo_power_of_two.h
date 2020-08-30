da8f234ddd8cbe1f6b32a4f9fb8f8b9d6b5b0d78
FengShun <fengshun95@gmail.com>
2020-08-31 07:38:59 +0800

    Fix and optimize fifo class

--- include/ulog/helper/fifo_power_of_two.h
+++ include/ulog/helper/fifo_power_of_two.h
@@ -17,46 +17,32 @@ class FifoPowerOfTwo {
  public:
   FifoPowerOfTwo(void *buffer, size_t buf_size, size_t element_size = 1)
       : data_((unsigned char *)buffer),
-        element_size_(element_size),
-        in_(0),
-        out_(0),
+        element_size_(element_size != 0 ? element_size : 1),
         is_allocated_memory_(false) {
-    if (element_size == 0) {
-      mask_ = 0;
-      data_ = nullptr;
-      return;
-    }
-    size_t num_elements = buf_size / element_size;
-
-    // round down to the next power of 2, since our 'let the indices wrap'
-    // technique works only in this case.
-    // The kernel is roundup_pow_of_two, i think it is wrong.
-    num_elements = RounddownPowOfTwo(num_elements);
+    size_t num_elements = buf_size / element_size_;
 
     if (num_elements < 2) {
       mask_ = 0;
       data_ = nullptr;
       return;
     }
-    mask_ = num_elements - 1;
+
+    // round down to the next power of 2, since our 'let the indices wrap'
+    // technique works only in this case.
+    // The kernel is roundup_pow_of_two, maybe it's wrong.
+    mask_ = RoundDownPowOfTwo(num_elements) - 1;
   }
 
   explicit FifoPowerOfTwo(size_t num_elements, size_t element_size = 1)
-      : element_size_(element_size),
-        in_(0),
-        out_(0),
+      : element_size_(element_size != 0 ? element_size : 1),
         is_allocated_memory_(true) {
+    if (num_elements < 2) num_elements = 2;
+
     // round up to the next power of 2, since our 'let the indices wrap'
     // technique works only in this case.
-    num_elements = RoundupPowOfTwo(num_elements);
+    num_elements = RoundUpPowOfTwo(num_elements);
 
-    if (num_elements < 2 || element_size == 0) {
-      mask_ = 0;
-      data_ = nullptr;
-      return;
-    }
-
-    data_ = new unsigned char[num_elements * element_size];
+    data_ = new unsigned char[num_elements * element_size_];
 
     if (!data_) {
       mask_ = 0;
@@ -74,15 +60,14 @@ class FifoPowerOfTwo {
   // A packet is entered, either completely written or discarded.
   size_t InPacket(const void *buf, size_t num_elements) {
     LockGuard lg(mutex_);
-    size_t unused = Unused();
 
-    if (unused < num_elements) {
+    if (available() < num_elements) {
       num_dropped_ += num_elements;
       peak_ = size();
       return 0;
     }
 
-    peak_ = Max(peak_, Used());
+    peak_ = max(peak_, used());
 
     CopyInLocked(buf, num_elements, in_);
     in_ += num_elements;
@@ -94,17 +79,16 @@ class FifoPowerOfTwo {
   size_t In(const void *buf, size_t num_elements) {
     LockGuard lg(mutex_);
 
-    peak_ = Max(peak_, Used());
+    peak_ = max(peak_, used());
 
-    size_t unused = Unused();
-    num_elements = Min(num_elements, unused);
+    const size_t len = min(num_elements, available());
 
-    CopyInLocked(buf, num_elements, in_);
-    in_ += num_elements;
-    num_dropped_ += unused - num_elements;
+    CopyInLocked(buf, len, in_);
+    in_ += len;
+    num_dropped_ += num_elements - len;
 
     cond_.Sign();
-    return num_elements;
+    return len;
   }
 
   /**
@@ -119,7 +103,7 @@ class FifoPowerOfTwo {
     out_ = in_;
   }
 
-  bool full() const { return Used() == size(); }
+  bool full() const { return used() == size(); }
   bool empty() const { return in_ == out_; }
 
   /**
@@ -127,14 +111,14 @@ class FifoPowerOfTwo {
    * Return %true if fifo is initialized, otherwise %false.
    * Assumes the fifo was 0 before.
    */
-  bool Initialized() const { return mask_ != 0; };
+  bool initialized() const { return mask_ != 0; };
   size_t num_dropped() const { return num_dropped_; }
   size_t peak() const { return peak_; }
   size_t size() const { return mask_ + 1; }
 
   size_t OutPeek(void *out_buf, size_t num_elements) {
     LockGuard lg(mutex_);
-    num_elements = Min(num_elements, Used());
+    num_elements = min(num_elements, used());
     CopyOutLocked(out_buf, num_elements, out_);
     return num_elements;
   }
@@ -150,7 +134,7 @@ class FifoPowerOfTwo {
       if (empty()) return 0;
     }
 
-    num_elements = Min(num_elements, Used());
+    num_elements = min(num_elements, used());
     CopyOutLocked(out_buf, num_elements, out_);
     out_ += num_elements;
     return num_elements;
@@ -158,19 +142,19 @@ class FifoPowerOfTwo {
 
   size_t Out(void *out_buf, size_t num_elements) {
     LockGuard lg(mutex_);
-    num_elements = Min(num_elements, Used());
+    num_elements = min(num_elements, used());
     CopyOutLocked(out_buf, num_elements, out_);
     out_ += num_elements;
     return num_elements;
   }
 
  private:
-  size_t in_{};               // data is added at offset (in % size)
-  size_t out_{};              // data is extracted from off. (out % size)
-  unsigned char *data_;       // the buffer holding the data
-  bool is_allocated_memory_;  //  Used to identify whether the internal buffer
-                              //  is allocated internally
-  size_t mask_;  // (Constant) Mask used to match the correct in / out pointer
+  size_t in_{};                      // data is added at offset (in % size)
+  size_t out_{};                     // data is extracted from off. (out % size)
+  unsigned char *data_{};            // the buffer holding the data
+  bool is_allocated_memory_{false};  // Used to identify whether the internal
+                                     // buffer is allocated internally
+  size_t mask_{};  // (Constant) Mask used to match the correct in / out pointer
   const size_t element_size_;  // the size of the element
   size_t num_dropped_{};       // Number of dropped elements
   size_t peak_{};              // fifo peak
@@ -184,8 +168,8 @@ class FifoPowerOfTwo {
 
    private:
     pthread_mutex_t mutex_{};
-  } mutex_;  // TODO: If there is only one input and one output thread, then
-             // locks are not necessary
+  } mutex_{};  // TODO: If there is only one input and one output thread, then
+               // locks are not necessary
 
   class Condition {
    public:
@@ -220,7 +204,7 @@ class FifoPowerOfTwo {
     }
     pthread_cond_t cond_{};
     static constexpr clockid_t clockid_ = CLOCK_REALTIME;
-  } cond_;
+  } cond_{};
 
   class LockGuard {
    public:
@@ -240,7 +224,7 @@ class FifoPowerOfTwo {
       size *= element_size_;
       len *= element_size_;
     }
-    size_t l = Min(len, size - off);
+    size_t l = min(len, size - off);
 
     memcpy(data_ + off, src, l);
     memcpy(data_, (unsigned char *)src + l, len - l);
@@ -256,51 +240,56 @@ class FifoPowerOfTwo {
       size *= element_size_;
       len *= element_size_;
     }
-    l = Min(len, size - off);
+    l = min(len, size - off);
 
     memcpy(dst, data_ + off, l);
     memcpy((unsigned char *)dst + l, data_, len - l);
   }
 
-  size_t Unused() const { return size() - Used(); }
-  size_t Used() const { return in_ - out_; }
+  size_t available() const { return size() - used(); }
+  size_t used() const { return in_ - out_; }
 
   template <typename T>
-  static inline T Min(T x, T y) {
+  static inline T min(T x, T y) {
     return x < y ? x : y;
   }
   template <typename T>
-  static inline T Max(T x, T y) {
+  static inline T max(T x, T y) {
     return x > y ? x : y;
   }
 
   // round up to nearest power of two
-  static inline unsigned long RoundupPowOfTwo(unsigned long n) {
-    return 1UL << (unsigned int)fls(n - 1U);
+  static inline uint64_t RoundUpPowOfTwo(uint64_t n) {
+    return (n == 0) ? 1 : (1UL << FindLastSet(n - 1U));
   }
 
   // round down to nearest power of two
-  static inline unsigned long RounddownPowOfTwo(unsigned long n) {
-    return 1UL << (fls(n) - 1U);
+  static inline uint64_t RoundDownPowOfTwo(uint64_t n) {
+    return (n == 0) ? 1 : (1UL << (FindLastSet(n) - 1U));
   }
 
-  // fls: find last bit set.
-  static inline int fls(uint64_t x) { return 64 - clz64(x); }
+  // Contrary to __builtin_ffs, find the highest 1
+  // if x is zero, returns zero.
+  static inline unsigned FindLastSet(uint64_t x) {
+    return 64 - CountLeadingZero(x);
+  }
 
-  // Returns the number of leading 0-bits in x, starting at the most significant
-  // bit position. If x is 0, the result is undefined.
-  static inline int clz64(uint64_t x) {
-    int r = 0;
+  // Similar to __builtin_clz:
+  // Returns the number of leading 0-bits in x, starting at the most
+  // significant bit position.
+  static inline unsigned CountLeadingZero(uint64_t x) {
+    unsigned r = 0;
     if (!(x & 0xFFFFFFFF00000000)) r += 32, x <<= 32U;
     if (!(x & 0xFFFF000000000000)) r += 16, x <<= 16U;
     if (!(x & 0xFF00000000000000)) r += 8, x <<= 8U;
     if (!(x & 0xF000000000000000)) r += 4, x <<= 4U;
     if (!(x & 0xC000000000000000)) r += 2, x <<= 2U;
+    if (!(x & 0x8000000000000000)) r += 1, x <<= 1U;
     if (!(x & 0x8000000000000000)) r += 1;
     return r;
   }
 };
 
-};  // namespace ulog
+}  // namespace ulog
 
 #endif  // ULOG_INCLUDE_ULOG_FIFOPOWEROF2_H_
