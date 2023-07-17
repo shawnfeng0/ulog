b63a6065fdf49e7df1112480c51fc632414e4d90
Shawn Feng <shawnfeng0@gmail.com>
2023-07-18 03:38:56 +0800

    Independently test the code related to power_of_2

--- tests/bip_buffer_all_test.cc
+++ tests/bip_buffer_all_test.cc
@@ -12,27 +12,6 @@
 #include "ulog/helper/queue/spsc_bip_buffer2.h"
 #include "ulog/ulog.h"
 
-TEST(BipBuffer, IsInRange) {
-  ASSERT_EQ(ulog::queue::IsInRange(10, 9, 20), false);
-  ASSERT_EQ(ulog::queue::IsInRange(10, 10, 20), true);
-  ASSERT_EQ(ulog::queue::IsInRange(10, 11, 20), true);
-
-  ASSERT_EQ(ulog::queue::IsInRange(10, 19, 20), true);
-  ASSERT_EQ(ulog::queue::IsInRange(10, 20, 20), true);
-  ASSERT_EQ(ulog::queue::IsInRange(10, 21, 20), false);
-
-  ASSERT_EQ(ulog::queue::IsInRange(10, 9, 10), false);
-  ASSERT_EQ(ulog::queue::IsInRange(10, 10, 10), true);
-  ASSERT_EQ(ulog::queue::IsInRange(10, 11, 10), false);
-
-  ASSERT_EQ(ulog::queue::IsInRange(UINT32_MAX - 1, UINT32_MAX - 2, 1), false);
-  ASSERT_EQ(ulog::queue::IsInRange(UINT32_MAX - 1, UINT32_MAX - 1, 1), true);
-  ASSERT_EQ(ulog::queue::IsInRange(UINT32_MAX - 1, UINT32_MAX, 1), true);
-  ASSERT_EQ(ulog::queue::IsInRange(UINT32_MAX - 1, 0, 1), true);
-  ASSERT_EQ(ulog::queue::IsInRange(UINT32_MAX - 1, 1, 1), true);
-  ASSERT_EQ(ulog::queue::IsInRange(UINT32_MAX - 1, 2, 1), false);
-}
-
 template <typename T>
 static void spsc(uint32_t buffer_size) {
   const uint32_t limit = buffer_size * 8192;
